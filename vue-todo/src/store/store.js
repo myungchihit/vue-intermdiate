6c642892a0fb1e3087a935c0d437863d076d8524
JJangGyu <pridom112@naver.com>
2022-06-12 07:20:24 +0900

    finally vuex modularity

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,29 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import * as getters from './getters';
-import * as mutations from './mutations';
-
+// import * as getters from './getters';  // 속셩별 구조화 방법
+// import * as mutations from './mutations';
+import todoApp from './modules/todoApp';
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if(localStorage.length > 0){
-            for (let i = 0; i < localStorage.length; i++) {
-                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    },
-}
+// 속성별 구조화 했을때.
+// export const store = new Vuex.Store({
+//     state: {
+//         todoItems: storage.fetch(),
+//     },
+//     getters,
+//     mutations
+// });
 
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch(),
-    },
-    getters,
-    mutations
+    modules: {
+        todoApp
+    }
 });
